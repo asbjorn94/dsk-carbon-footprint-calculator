09c00fd2b8b92f4e9bd6147825b19ba1ff526f57
Asbjørn Grønbæk <asbjorn@gronbak.dk>
2025-01-16 19:44:41 +0000

    Initial structure for web app.

---
+++ main.py
@@ -0,0 +1,39 @@
+from flask import request, jsonify
+from config import app, db
+from models import Contact
+
+with app.app_context():
+    db.create_all()
+
+@app.route("/contacts", methods=["GET"])
+def get_contacts():
+    contacts = Contact.query.all()
+    json_contacts = list(map(lambda x: x.to_json(), contacts))
+    print("Called get_contacts")
+    return jsonify({"contacts": json_contacts})
+
+@app.route("/get_footprint", methods=["POST"])
+def create_contact():
+    first_name = request.json.get("firstName")
+    last_name = request.json.get("lastName")
+    email = request.json.get("email")
+
+    if not first_name or not last_name or not email:
+        return (
+            jsonify({"message": "You must include a first name, last name and email"}),400
+        )
+
+    new_contact = Contact(first_name=first_name, last_name=last_name, email=email)
+    try:
+        db.session.add(new_contact)
+        db.session.commit()
+    except Exception as e:
+        return jsonify({"message": str(e)}), 400
+
+    return jsonify({"message": "User created!"}), 201
+
+# if __name__ == "__main__":
+#     with app.app_context():
+#         db.create_all()
+
+#     app.run(debug=True)

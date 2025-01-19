ed0d28c38a9d1dc14bd9e6322ec47ddd78ba3566
Asbjørn Grønbæk <asbjorn@gronbak.dk>
2025-01-19 21:57:23 +0100

    Changed so os.path to get CSV database is handled correctly

--- panda_database.py
+++ panda_database.py
@@ -1,8 +1,11 @@
 import os
 from pandas import read_csv
 
-basedir = os.getcwd()
+# basedir = os.getcwd()
+basedir = os.path.dirname(__file__)
+print("basedir: " + basedir)
 full_path = os.path.join(basedir,"res/DSK_tiny.csv")    
+print("full_path: " + full_path)
 panda_db = read_csv(full_path, sep=";")
 
 

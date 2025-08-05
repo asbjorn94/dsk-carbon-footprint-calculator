1c5344d3c2a1af6eb6e499fe5dd4475999a9100c
Asbjørn Grønbæk <asbjorn@gronbak.dk>
2025-08-05 11:32:31 +0200

    Removed some old tutorial code.

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 from flask import request, jsonify
 from config import app, db
-from models import Contact #Should eventually be removed, but kept for testing purposes
-
 import json
 from utils import Utils
 
@@ -29,42 +27,6 @@ def get_footprint():
 
 # with app.app_context():
 #     db.create_all()
-
-# @app.route("/contacts", methods=["GET"])
-# def get_contacts():
-#     contacts = Contact.query.all()
-#     json_contacts = list(map(lambda x: x.to_json(), contacts))
-#     return jsonify({"contacts": json_contacts})
-
-# @app.route("/create_contact", methods=["POST"])
-# def create_contact():
-#     first_name = request.json.get("firstName")
-#     last_name = request.json.get("lastName")
-#     email = request.json.get("email")
-
-#     if not first_name or not last_name or not email:
-#         return (
-#             jsonify({"message": "You must include a first name, last name and email"}),400
-#         )
-
-#     new_contact = Contact(first_name=first_name, last_name=last_name, email=email)
-#     try:
-#         db.session.add(new_contact)
-#         db.session.commit()
-#     except Exception as e:
-#         return jsonify({"message": str(e)}), 400
-
-#     return jsonify({"message": "User created!"}), 201
-
-# @app.route("/add_five", methods=["POST"])
-# def add_five():
-#     number = request.json.get("value")
-#     return_value = int(number) + 5
-
-#     return jsonify({"return_value": return_value}), 201
-
-
-
 
 # if __name__ == "__main__":
 #     with app.app_context():
